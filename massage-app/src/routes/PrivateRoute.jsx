b85d38399db9b8885173e24601cd58c6b6e6e4f4
blovelace84 <blovelace84@gmail.com>
2025-06-17 09:11:15 -0400

    Finally fixed the Private Route issue

--- massage-app/src/routes/PrivateRoute.jsx
+++ massage-app/src/routes/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/useAuth";
+import { useAuth } from "../context/AuthContext"; // ✅ correct import
 import { children } from "react";
 
 const PrivateRoute = ({ Children }) => {
